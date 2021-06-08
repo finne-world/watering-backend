c97a285c7294acad1d197f05871e25a7888393ee
Mineshima Kazuki <finne@MineshimanoMacBook-Pro.local>
2021-06-08 21:20:07 +0900

    remove flyway

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -4,5 +4,4 @@ import org.gradle.plugin.use.PluginDependenciesSpec
 val PluginDependenciesSpec.jvm get() = kotlin("jvm").version(Versions.Plugin.KOTLIN)
 val PluginDependenciesSpec.boot get() = id("org.springframework.boot").version(Versions.Plugin.SPRING_BOOT)
 val PluginDependenciesSpec.dependencyManagement get() = id("io.spring.dependency-management").version(Versions.Plugin.DEPENDENCY_MANAGEMENT)
-val PluginDependenciesSpec.flyway get() = id("org.flywaydb.flyway").version(Versions.Plugin.FLYWAY)
 val PluginDependenciesSpec.spring get() = kotlin("plugin.spring").version(Versions.Plugin.KOTLIN)
