f1217a9cd8bf5336ce38995c6df3c31cb4d3153a
Mineshima Kazuki <finne@MineshimanoMacBook-Pro.local>
2021-06-20 18:58:29 +0900

    refactoring

--- src/main/kotlin/com/watering/watering_backend/application/json/response/auto_watering/ChangeEnabledResponse.kt
+++ src/main/kotlin/com/watering/watering_backend/application/json/response/auto_watering/ChangeEnabledResponse.kt
@@ -1,11 +1,12 @@
-package com.watering.watering_backend.application.json.response
+package com.watering.watering_backend.application.json.response.auto_watering
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.watering.watering_backend.application.json.response.OkResponse
 import com.watering.watering_backend.application.json.response.`object`.UpdatedValue
 import java.util.UUID
 
 @JsonPropertyOrder("status_code", "timestamp", "device_id", "enabled")
-class AutoWateringResponse(
+class ChangeEnabledResponse(
     val deviceId: UUID,
     val enabled: UpdatedValue<Boolean>,
 ): OkResponse()
--- src/main/kotlin/com/watering/watering_backend/application/json/response/device/RegisterDeviceResponse.kt
+++ src/main/kotlin/com/watering/watering_backend/application/json/response/device/RegisterDeviceResponse.kt
@@ -1,12 +1,13 @@
-package com.watering.watering_backend.application.json.response
+package com.watering.watering_backend.application.json.response.device
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.watering.watering_backend.application.json.response.OkResponse
 import com.watering.watering_backend.application.json.response.`object`.AutoWateringSetting
 import com.watering.watering_backend.application.json.response.`object`.WateringSetting
 import java.util.UUID
 
 @JsonPropertyOrder("status_code", "timestamp", "device_id", "watering_setting", "auto_watering_setting")
-class DevicesAddResponse(
+class RegisterDeviceResponse(
     val deviceId: UUID,
     val wateringSetting: WateringSetting,
     val autoWateringSetting: AutoWateringSetting
